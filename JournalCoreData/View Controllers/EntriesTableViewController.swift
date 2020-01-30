3f8523ddbb5a8f08bd3984fbd30e7b37c9f4eaa6
Craig Swanson <cswanson02@me.com>
2020-01-29 21:35:22 -0600

    Updated a fix comment that I had previously forgotten to comment.

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -14,6 +14,7 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // FIXED: added code to fetch the entries from server.
         entryController.fetchEntriesFromServer()
         tableView.reloadData()
         
